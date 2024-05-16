5fb3f816db7bda7f3d50ecffac728b2e7339c8a3
Camila Arce <arce.b.camila@gmail.com>
2024-05-16 11:23:38 -0300

    modificar index.html y contacto.html

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -13,30 +13,3 @@ function changebtn() {
         btn_menu.classList.replace("bx-menu-alt-right", "bx-menu");
     }
 }
-
-const btn_theme = document.querySelector(".theme-btn");
-const theme_ball = document.querySelector(".theme-ball");
-
-const localData = localStorage.getItem("theme");
-
-if (localData == null) {
-    localStorage.setItem("theme", "light");
-}
-
-if (localData == "dark") {
-    document.body.classList.add("dark-mode");
-    theme_ball.classList.add("dark");
-} else if (localData == "light") {
-    document.body.classList.remove("dark-mode");
-    theme_ball.classList.remove("dark");
-}
-
-btn_theme.addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
-    theme_ball.classList.toggle("dark");
-    if (document.body.classList.contains("dark-mode")) {
-        localStorage.setItem("theme", "dark");
-    } else {
-        localStorage.setItem("theme", "light");
-    }
-});
