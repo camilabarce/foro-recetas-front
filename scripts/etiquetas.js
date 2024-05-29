0c1f940ce82a9af9c88d21d03cebba44566a56c0
Asaldi98 <sagustinlujan@gmail.com>
2024-05-29 02:42:12 -0300

    modificado scripts de new-post

--- scripts/etiquetas.js
+++ scripts/etiquetas.js
@@ -1,37 +1,44 @@
 const ul = document.getElementById("etiquetas"),
-input = document.getElementById("tagInput"),
-contador = document.querySelector(".detalles span");
+    input = document.getElementById("tagInput"),
+    contador = document.querySelector(".detalles span");
 
 let maxTag = 4, tags = [];
 
 contadorTag();
 
-function contadorTag(){
+document.getElementById('crearPost').addEventListener('keypress', function (e) {
+    if (e.key == 13) {
+        e.preventDefault();
+    }
+});
+
+function contadorTag() {
     input.focus();
     contador.innerText = maxTag - tags.length;
 }
 
-function createTag(){
-    ul.querySelectorAll("li").forEach(li => {li.remove()});
+function createTag() {
+    ul.querySelectorAll("li").forEach(li => { li.remove() });
     tags.slice().reverse().forEach(tag => {
-        let liTag = `<li class="list-group-item m-1">${tag} <i class="bi bi-x-circle" onclick="remove(this,'${tag}')"></i></li>`;
-        ul.insertAdjacentHTML("afterbegin",liTag);
+        let liTag = `<li class="list-group-item m-1" onclick="remove(this,'${tag}')">${tag} <i id="botonCerrar" class="bi bi-x-circle"></i></li>`;
+        ul.insertAdjacentHTML("afterbegin", liTag);
+        botonEtiqueta();
     })
     contadorTag();
 }
 
-function remove(element, tag){
+function remove(element, tag) {
     let index = tags.indexOf(tag);
     tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
-    element.parentElement.remove();
+    element.remove();
     contadorTag();
 }
 
-function addTag(e){
-    if(e.key == " " || e.key == ","){
-        let tag = e.target.value.replaceAll(",","").replaceAll(" ","");
-        if(tag.length>1 && !tags.includes(tag)){
-            if(tags.length < maxTag){
+function addTag(e) {
+    if (e.key == " " || e.key == ",") {
+        let tag = e.target.value.replaceAll(",", "").replaceAll(" ", "");
+        if (tag.length > 1 && !tags.includes(tag)) {
+            if (tags.length < maxTag) {
                 tags.push(tag);
                 createTag();
             }
@@ -40,10 +47,23 @@ function addTag(e){
     }
 }
 
-input.addEventListener("keyup",addTag);
+input.addEventListener("keyup", addTag);
 
-function reset(){
+function botonEtiqueta() {
+    const equis = document.getElementById("botonCerrar");
+
+    equis.parentElement.onmouseover = () => {
+        equis.className = "bi bi-x-circle-fill";
+        equis.parentElement.style.cursor = 'pointer';
+    };
+    equis.parentElement.onmouseout = () => {
+        equis.className = "bi bi-x-circle";
+    };
+
+}
+
+function reset() {
     tags.length = 0;
-    ul.querySelectorAll("li").forEach(li => {li.remove()});
+    ul.querySelectorAll("li").forEach(li => { li.remove() });
     contadorTag();
 }
